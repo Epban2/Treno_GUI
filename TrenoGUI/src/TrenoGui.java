2751b82997f688794bd23ace9e4076af141ccaf7
Riccardo La Rocca <sriccardo.larocca@itis.pr.it>
2024-02-18 12:52:52 +0100

    tentando di aggiungere le immagini

--- TrenoGUI/src/TrenoGui.java
+++ TrenoGUI/src/TrenoGui.java
@@ -1,3 +1,4 @@
+
 /**
 Commento
 */
@@ -8,19 +9,22 @@ import java.awt.event.ActionListener;
 
 public class TrenoGui extends JFrame {
     public Treno treno;
-    private JLabel jlbTitolo; //etichetta del titolo
-    private JLabel jlbTesto; //etichetta di testo generica
-    private JPanel jpnTitolo; //pannello del titolo
-    private JPanel jpnVagoni; //pannello con le img dei vagoni
+    private JLabel jlbTitolo; // etichetta del titolo
+    private JLabel jlbTesto; // etichetta di testo generica
+    private JPanel jpnTitolo; // pannello del titolo
+    public JPanel jpnVagoni; // pannello con le img dei vagoni
 
-    private JPanel jpnBottoniLaterali; //pannello con i bottoni per aggiungere vagoni (laterale)
+    // #
+    private JPanel jpnBottoniLaterali; // pannello con i bottoni per aggiungere vagoni (laterale)
 
-    private JButton jbVagoneMerci; //bottone per aggiungere vagone merci
-    private JButton jbVagonePasseggeri; //vagone passeggeri
+    private JButton jbVagoneMerci; // bottone per aggiungere vagone merci
+    private JButton jbVagonePasseggeri; // vagone passeggeri
 
+    private JLabel jlbTestoPasseggeri;
+    private JLabel jlbTestoMerci;
 
     public TrenoGui(Treno trenoParametro) {
-        this.treno=trenoParametro;
+        this.treno = trenoParametro;
         this.setSize(1000, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
@@ -37,19 +41,35 @@ public class TrenoGui extends JFrame {
         jpnVagoni = new JPanel();
         jpnVagoni.setLayout(new FlowLayout());
 
-        jlbTesto = new JLabel();
-        jlbTesto.setText("Aggiungi vagone merci");
-        jlbTesto.setForeground(Color.BLACK);
-        jlbTesto.setFont(new Font("Futura", Font.BOLD, 20));
-        jlbTesto.setHorizontalAlignment(JLabel.CENTER);
+        // aggiungi vagone merci
+        jlbTestoMerci = new JLabel();
+        jlbTestoMerci.setText("Aggiungi vagone merci");
+        jlbTestoMerci.setForeground(Color.BLACK);
+        jlbTestoMerci.setFont(new Font("Futura", Font.BOLD, 17));
+        jlbTestoMerci.setHorizontalAlignment(JLabel.CENTER);
 
         jbVagoneMerci = new JButton();
-        jbVagoneMerci.add(jlbTesto);
+        jbVagoneMerci.add(jlbTestoMerci);
         jbVagoneMerci.addActionListener(new AscoltaVagoneMerci());
 
-        jpnBottoniLaterali = new JPanel();
-        jpnBottoniLaterali.add(jbVagoneMerci);
+        // vagone passeggeri
+        jbVagonePasseggeri = new JButton();
+        jlbTestoPasseggeri = new JLabel();
+        jlbTestoPasseggeri.setText("Aggiungi vagone passeggeri");
+        jlbTestoPasseggeri.setForeground(Color.BLACK);
+        jlbTestoPasseggeri.setFont(new Font("Futura", Font.BOLD, 17));
+        jlbTestoPasseggeri.setHorizontalAlignment(JLabel.CENTER);
 
+        jbVagonePasseggeri.add(jlbTestoPasseggeri);
+        jbVagonePasseggeri.addActionListener(new AscoltaVagonePasseggeri());
+
+        // aggiungi bottoni al pannello
+        jpnBottoniLaterali = new JPanel();
+        jpnBottoniLaterali.setLayout(new BoxLayout(jpnBottoniLaterali, BoxLayout.Y_AXIS));
+        jpnBottoniLaterali.add(jbVagoneMerci);
+        jpnBottoniLaterali.add(jbVagonePasseggeri);
+
+        // frame
         this.add(jpnTitolo, BorderLayout.NORTH);
         this.add(jpnBottoniLaterali, BorderLayout.EAST);
         this.add(jpnVagoni, BorderLayout.CENTER);
@@ -57,9 +77,23 @@ public class TrenoGui extends JFrame {
     }
 
     /**
-     * Bottone Reset
+     * Event listener di vagone merci button
      */
     class AscoltaVagoneMerci implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JLabel label = new JLabel();
+            label.setIcon(new ImageIcon("TrenoGUI/img/vagone_merci.jpg"));
+
+            jpnVagoni.add(label);
+
+        }
+    }
+
+    /**
+     * Event listener di vagone passeggeri button
+     */
+    class AscoltaVagonePasseggeri implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
